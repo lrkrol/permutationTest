f449c7377ff7a611ab360acf7bf35efb837925ab
Laurens R Krol <lrkrol+git@gmail.com>
2017-06-07 14:55:26 +0200

    Initial commit

---
+++ permutationTest.m
@@ -0,0 +1,125 @@
+% [p, observeddifference, effectsize] = permutationTest(sample1, sample2, permutations [, varargin])
+%
+% In:
+%       sample1 - vector of measurements representing one condition
+%       sample2 - vector of measurements representing a second condition
+%       permutations - the number of permutations
+%
+% Optional (name-value pairs):
+%       sidedness - whether to test one- or two-sided:
+%           'both' - test two-sided (default)
+%           'smaller' - test one-sided, alternative hypothesis is that
+%                       the mean of sample1 is smaller than the mean of
+%                       sample2
+%           'larger' - test one-sided, alternative hypothesis is that
+%                      the mean of sample1 is larger than the mean of
+%                      sample2
+%       plotresult - whether or not to plot the distribution of randomised
+%                    differences, along with the observed difference (1|0,
+%                    default: 0)
+%       showprogress - whether or not to show a progress bar. if 0, no bar
+%                      is displayed; if showprogress > 0, the bar updates 
+%                      every showprogress-th iteration.
+%
+% Out:  
+%       p - the resulting p-value
+%       observeddifference - the observed difference between the two
+%                            samples, i.e. mean(sample1) - mean(sample2)
+%       effectsize - the effect size
+%
+% Usage example:
+%       >> permutationTest(rand(1,100), rand(1,100)-.25, 10000, ...
+%          'plotresult', 1, 'showprogress', 250)
+% 
+%                       Copyright 2015-2017 Laurens R Krol
+%                       Team PhyPA, Biological Psychology and Neuroergonomics,
+%                       Berlin Institute of Technology
+
+% 2017-04-04 lrk
+%   - Added progress bar
+% 2017-01-13 lrk
+%   - Switched to inputParser to parse arguments
+% 2016-09-13 lrk
+%   - Caught potential issue when column vectors were used
+%   - Improved plot
+% 2016-02-17 toz
+%   - Added plot functionality
+% 2015-11-26 First version
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+function [p, observeddifference, effectsize] = permutationTest(sample1, sample2, permutations, varargin)
+
+% parsing input
+p = inputParser;
+
+addRequired(p, 'sample1', @isnumeric);
+addRequired(p, 'sample2', @isnumeric);
+addRequired(p, 'permutations', @isnumeric);
+
+addParamValue(p, 'sidedness', 'both', @(x) any(validatestring(x,{'both', 'smaller', 'larger'})));
+addParamValue(p, 'plotresult', 0, @isnumeric);
+addParamValue(p, 'showprogress', 0, @isnumeric);
+
+parse(p, sample1, sample2, permutations, varargin{:})
+
+sample1 = p.Results.sample1;
+sample2 = p.Results.sample2;
+permutations = p.Results.permutations;
+sidedness = p.Results.sidedness;
+plotresult = p.Results.plotresult;
+showprogress = p.Results.showprogress;
+
+if iscolumn(sample1), sample1 = sample1'; end
+if iscolumn(sample2), sample2 = sample2'; end
+
+% running test
+allobservations = [sample1, sample2];
+randomdifferences = zeros(1, permutations);
+if showprogress, w = waitbar(0, '', 'Name', 'permutationTest'); end
+for n = 1:permutations
+    if showprogress, if mod(n,showprogress) == 0, waitbar(n/permutations, w, sprintf('Permutation %d of %d', n, permutations)); end; end
+    
+    permutation = randperm(length(allobservations));
+    randomSample1 = allobservations(permutation(1:length(sample1)));
+    randomSample2 = allobservations(permutation(length(sample1)+1:length(permutation)));
+    
+    randomdifferences(n) = mean(randomSample1) - mean(randomSample2);
+end
+if showprogress, delete(w); end
+
+observeddifference = mean(sample1) - mean(sample2);
+effectsize = observeddifference / mean([std(sample1), std(sample2)]);
+
+if strcmp(sidedness, 'both')
+    p = (length(find(abs(randomdifferences) > abs(observeddifference)))+1) / (permutations+1);
+elseif strcmp(sidedness, 'smaller')
+    p = (length(find(randomdifferences < observeddifference))+1) / (permutations+1);
+elseif strcmp(sidedness, 'larger')
+    p = (length(find(randomdifferences > observeddifference))+1) / (permutations+1);
+end
+
+% plotting result
+if plotresult
+    figure;
+    hist(randomdifferences);
+    hold on;
+    xlabel('Random differences');
+    ylabel('Count')
+    od = plot(observeddifference, 0, '*r', 'DisplayName', sprintf('Observed difference:\neffect size: %.2f,\np = %f', effectsize, p));
+    legend(od);
+end
+
+end
